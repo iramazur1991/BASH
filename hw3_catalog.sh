c093142fbb3144dfef83af1f731daa962adf2ed8
Iryna Lienska <46135805+iramazur1991@users.noreply.github.com>
2022-07-23 11:52:29 +0100

    Add files via upload

---
+++ hw3_catalog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p /home/Lienska
+mkdir -p /home/TBB/
+mkdir -p /home/TDK/
+mkdir -p /home/TBB/GR1
+mkdir -p /home/TDK/GR2
+touch /home/TBB/GR1/Ira.txt
+touch /home/TDK/GR2/Lienska.txt
+echo “iramazur1991@gmail.com” > Ira.txt
+echo “2022 7/22/2022” > Lienska.txt
+cp Ira.txt Lienska.txt /home
